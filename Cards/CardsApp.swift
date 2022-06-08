f13d7801961a77f6d04651252baf54eaf890e109
MKFLA <mkfla.work@gmail.com>
2022-06-08 17:00:21 +0800

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by shuo zhang on 2022/6/8.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
