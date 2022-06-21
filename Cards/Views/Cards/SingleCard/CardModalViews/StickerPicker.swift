9434ec0f7269ac718559e80c12baf2d51ada1693
MKFLA <mkfla.work@gmail.com>
2022-06-21 16:49:39 +0800

    用引用的方式读取stickers

--- Cards/Views/Cards/SingleCard/CardModalViews/StickerPicker.swift
+++ Cards/Views/Cards/SingleCard/CardModalViews/StickerPicker.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct StickerPicker: View {
+  @State private var stickerNames: [String] = []
+  
   var body: some View {
-    if let resourcePath = Bundle.main.resourcePath,
-       let uiImage = UIImage(named: resourcePath + "/Stickers/Camping/fire.png") {
-      Image(uiImage: uiImage)
-    } else {
-      EmptyView()
+    ScrollView {
+      ForEach(stickerNames, id: \.self) { path in
+        Image(uiImage: image(from: path))
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+      }
     }
+    .onAppear {
+      stickerNames = loadStickers()
+    }
+  }
+  
+  func loadStickers() -> [String] {
+    var themes: [URL] = []
+    var stickerNames: [String] = []
+    
+    let fileManager = FileManager.default
+    if let resourcePath = Bundle.main.resourcePath,
+       let enumerator = fileManager.enumerator(
+        at: URL(fileURLWithPath: resourcePath + "/Stickers"), includingPropertiesForKeys: nil,
+        options: [
+          .skipsHiddenFiles,
+          .skipsSubdirectoryDescendants
+        ])
+    {
+      for case let url as URL in enumerator {
+        if url.hasDirectoryPath {
+          themes.append(url)
+        }
+      }
+    }
+    
+    for theme in themes {
+      if let files = try? fileManager.contentsOfDirectory(atPath: theme.path) {
+        for file in files {
+          stickerNames.append(theme.path + "/" + file)
+        }
+      }
+    }
+    
+    return stickerNames
+  }
+  
+  func image(from path: String) -> UIImage {
+    print("loading:", path)
+    return UIImage(named: path)
+      ?? UIImage(named: "error-image")
+      ?? UIImage()
   }
 }
 
